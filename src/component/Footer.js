c347fbd2323f43f985942f89b897bb0c9a576b86
Nidhi Chaudhary <106021001+nidhichaudhary2002@users.noreply.github.com>
2023-06-24 15:08:08 +0530

    initial Commit

---
+++ src/component/Footer.js
@@ -0,0 +1,12 @@
+//routing
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>Copyright &copy ;2021</p>
+        <a href="/about"></a>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/component/Header.js
@@ -0,0 +1,25 @@
+import propTypes from 'prop-types'
+import Button from "./Button"
+
+const Header = ({title,onAdd,showAdd}) => {
+
+
+  return (
+    <header>
+        <h1 className='header'>
+           {title}
+        </h1>
+      <Button color={showAdd ?'red':'green'} text={showAdd ?'Close':'Add'} onClick={onAdd}/>
+     </header>
+  )
+}
+
+Header.defaultProps={
+    title:'Task Tracker',
+}
+
+Header.propTypes={
+   title:propTypes.string.isRequired,
+}
+
+export default Header
